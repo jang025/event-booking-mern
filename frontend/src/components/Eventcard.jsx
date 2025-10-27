b5883712bc506eb2b5300577a54be7dba2d301e5
junesutan <juneowns@gmail.com>
2025-10-27 13:38:41 +0800

    formatted date

--- frontend/src/components/Eventcard.jsx
+++ frontend/src/components/Eventcard.jsx
@@ -15,10 +15,31 @@ export default function Eventcard({ event }) {
     status,
   } = event;
 
+  //formats date to be shown on card to Sat, Nov 1, 9 AM
+  const formatted = new Date(start_time).toLocaleString("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: undefined,
+    hour12: true,
+  });
+
   return (
-    <div>
-      <p>{short_title}</p>
-      <p>{long_title}</p>
-    </div>
+    <>
+      <div style={{ display: "flex", gap: "20px" }}>
+        {/* image container */}
+        <div>
+          <p>image</p>
+        </div>
+        <div>
+          <h2>{short_title}</h2>
+          <p>{formatted}</p>
+          <p>{venue}</p>
+          <p>price</p>
+          <p>{status}</p>
+        </div>
+      </div>
+    </>
   );
 }
